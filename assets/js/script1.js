81360f69c02ef46306b0425dea6e5080132c5ae8
jonathan viera <jony.alejandro.viera@gmail.com>
2024-02-09 00:50:26 -0300

    :sparkles: version final con enlace a paginas secundarias

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -4,6 +4,24 @@ document.addEventListener('DOMContentLoaded', function() {
     const asuntoInput = document.getElementById('asunto');
     const mensajeInput = document.getElementById('mensaje');
     const mensajeResultado = document.querySelector('.resultado');
+    const secundariasDiv = document.getElementById('secundarias');
+
+    // Aplicar estilos CSS para centrar los enlaces
+    secundariasDiv.style.textAlign = 'center';
+    secundariasDiv.style.marginTop = '20px'; // Espacio arriba para separar del contenido principal
+
+    // Seleccionar los elementos <ul> y <li> dentro del div
+    const ul = secundariasDiv.querySelector('ul');
+    const lis = ul.querySelectorAll('li');
+
+    // Aplicar estilos a la lista y a los elementos de la lista
+    ul.style.listStyleType = 'none';
+    ul.style.padding = '0';
+
+    lis.forEach(li => {
+        li.style.display = 'inline-block'; // Mostrar en línea
+        li.style.marginRight = '20px'; // Espacio entre elementos
+    });
 
     formulario.addEventListener('submit', function(event) {
         event.preventDefault();
